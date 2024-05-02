87d07276e65092e4bb15fc8bad75cab03f72d964
Lucas Gautheron <lucas.gautheron@gmail.com>
2024-05-02 11:36:32 +0200

    delete=True

--- ale/cleaner.py
+++ ale/cleaner.py
@@ -151,7 +151,7 @@ class ArxivCleaner:
                         arxiv_id = proj_dir_or_file.stem
 
                         if not self.filter_func(arxiv_id):
-                            delete(proj_dir_or_file)
+                            delete(str(proj_dir_or_file))
                             continue
                         else:
                             print(f"processing {arxiv_id}")
@@ -159,7 +159,7 @@ class ArxivCleaner:
                         # load the tex source files (we also get the timestamp
                         # here)
                         data = _tex_proj_loader(proj_dir_or_file, self.filter_func)
-                        delete(proj_dir_or_file)
+                        delete(str(proj_dir_or_file))
 
                         if data is None:
                             failed += 1
